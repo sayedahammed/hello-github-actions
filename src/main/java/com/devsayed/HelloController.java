fcd9599545bc0a87ce03bef13ca30e014db36b31
sayedahammed <sayed.cse17@gmail.com>
2025-08-13 20:07:29 +0600

    first commit

---
+++ src/main/java/com/devsayed/HelloController.java
@@ -0,0 +1,15 @@
+package com.devsayed;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+public class HelloController {
+
+    @GetMapping
+    public String greetings() {
+        return "Hello!";
+    }
+}
